a7c2a3bc3ef09b6baf604ccf5eff595a66da87a2
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-27 18:28:27 +0700

    feat: add static path for upload and container injection

--- router/main.go
+++ router/main.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"noir-backend/container"
 	docs "noir-backend/docs"
 	"noir-backend/middleware"
 
@@ -9,15 +10,17 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func CombineRouter(r *gin.Engine) {
+func CombineRouter(r *gin.Engine, c *container.Container) {
 	docs.SwaggerInfo.BasePath = "/"
 	r.Use(middleware.CORS())
 	r.Use(middleware.ErrorHandler())
-	authRouter(r.Group("/auth"))
-	adminRouter(r.Group("/admin"))
-	userRouter(r.Group("/profile"))
-	movieRouter(r.Group("/movie"))
-	transactionRouter(r.Group("/transaction"))
+	r.Static("/uploads", "./uploads")
+
+	authRouter(r.Group("/auth"), c)
+	adminRouter(r.Group("/admin"), c)
+	userRouter(r.Group("/profile"), c)
+	movieRouter(r.Group("/movie"), c)
+	transactionRouter(r.Group("/transaction"), c)
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
