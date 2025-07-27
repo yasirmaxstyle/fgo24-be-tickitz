3e1827995aebb43b56217a9b04759cdd77eb3d7f
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-27 18:36:10 +0700

    feat: add feature for cancelling expired transaction

--- utils/transaction.go
+++ utils/transaction.go
@@ -1,11 +1,39 @@
 package utils
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+func StartTransactionExpiryJob(ctx context.Context, db *pgxpool.Pool) {
+	go func() {
+		ticker := time.NewTicker(1 * time.Minute)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("Transaction expiration job stopped")
+				return
+			case <-ticker.C:
+				_, err := db.Exec(context.Background(), `
+					UPDATE transactions 
+					SET status = 'expired' 
+					WHERE status = 'pending' AND expires_at < NOW()
+				`)
+				if err != nil {
+					fmt.Println("Failed to expire transactions:", err)
+				} else {
+					fmt.Println("Expired pending transactions (if any)")
+				}
+			}
+		}
+	}()
+}
 
 func GenerateTransactionCode() string {
 	return fmt.Sprintf("TXN-%d-%s", time.Now().Unix(), uuid.New().String()[:8])
