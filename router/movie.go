10defda7cf78fe4d7d7f9064db235efa774c7f36
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-27 18:29:00 +0700

    feat: add controller to handle endpoints

--- router/movie.go
+++ router/movie.go
@@ -1,15 +1,17 @@
 package router
 
 import (
+	"noir-backend/container"
 	"noir-backend/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
-func movieRouter(r *gin.RouterGroup) {
+func movieRouter(r *gin.RouterGroup, c *container.Container) {
 	r.Use(middleware.AuthMiddleware())
-	r.GET("/upcoming-movies")
-	r.GET("/now-playing-movies")
-	r.GET("/")       //get all movies
-	r.GET("/:id")    //get specific movie
+	r.GET("/upcoming-movies", c.MovieController.GetMoviesUpcoming)
+	r.GET("/now-playing-movies", c.MovieController.GetMoviesNowPlaying)
+	r.GET("/", c.MovieController.GetMovies)
+	r.GET("/:id", c.MovieController.GetMovieByID)
+	r.GET("/genres", c.MovieController.GetGenres)
 }
